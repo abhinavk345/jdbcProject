89c4d13cd1bedf276e25088ab69917a6da84244c
Abhinav <abhinav.kumar>
2020-07-20 17:08:16 +0530

    tricky_file

---
+++ com/test/Testing.java
@@ -0,0 +1,10 @@
+ package com.test;
+ class Testing {
+	   public static void main(String[] args){
+		  
+	    System.out.println(0.1*3 == 0.3);
+	    System.out.println(0.1*4 == 0.4);
+	    
+		}
+
+	 }
